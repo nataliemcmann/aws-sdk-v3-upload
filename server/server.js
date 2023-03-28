38796fa7000b532856715ed7fdea16699cd12476
Natalie McMann <nmcmann110@gmail.com>
2023-03-28 12:35:08 -0500

    added server.js boilerplate

---
+++ server/server.js
@@ -0,0 +1,20 @@
+//BOILERPLATE
+//imports to make the server work
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files
+app.use(express.static('build'));
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
